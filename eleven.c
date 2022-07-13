87f08ebd84393b317346baa0a99bb6d6499286ae
anshk1129 <109017321+anshk1129@users.noreply.github.com>
2022-07-13 11:00:28 +0530

    Add files via upload

---
+++ eleven.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+printf("enter the hour and minute ");
+scanf("%d %d",&a,&b);
+printf("%d HH  and %d MM ",a,b);
+return 0;
+}
---
+++ ten.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int a,b,c;
+printf("enter the day month and year");
+scanf("%d%d%d",&a,&b,&c);
+printf("Day \"–\" %d ,Month – %d , Year – %d",a,b,c);
+return 0;
+}
